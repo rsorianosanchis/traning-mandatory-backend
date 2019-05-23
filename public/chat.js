aa1f5c1e3ac67f6de071f8a8e87529854f7463cd
rsoriano <rsorianosanchis@gmail.com>
2019-05-23 23:34:55 +0200

    torsdag 23/052019 -- 23:34

---
+++ public/chat.js
@@ -0,0 +1,8 @@
+'use strict';
+let vPerson = document.getElementById('person'),
+    vAppChat= document.getElementById('app-chat'),
+    vWelcomeRutan= document.getElementById('welcome-rutan')
+
+function goIn(){
+  alert('hola');
+}
