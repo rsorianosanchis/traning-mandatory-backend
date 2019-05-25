e061448daa5e53a18ef47987380cf8cadc925cc4
rsoriano <rsorianosanchis@gmail.com>
2019-05-25 21:30:53 +0200

    lördag 25/05/2019--21:30-- integrera socket.io

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const socket = require('socket.io');
 const app = express();
 const port = 3001;
+
 //
 //
 const server = app.listen(port,()=>{
-  console.log(`Server kör i http://localhost:${server.address().port}`);
+  console.log(`Server kör på http://localhost:${server.address().port}`);
 });
+
 
 // app.get('/',(req,res)=>{
 //   res.send('server kommunikation is going on')
 // });
 /*med use använder man ett external dokument*/
-app.use(express.static('./public'))
+app.use(express.static('./public'));
+let io = socket(server);
+/*varje gång som man gör en connection ,socket skapas en ny id*/
+io.on('connection',(socket)=>{
+  console.log('det finns en connection i',socket.id);
+  // recieving msg and dela till alla
+  socket.on('chat',function(data){
+    console.log(data);
+    io.sockets.emit('chat',data);
+  })
+
+});
--- public/chat.js
+++ public/chat.js
@@ -1,8 +1,29 @@
 'use strict';
+/*Client side*/
+const socket = io.connect('http://localhost:3001/');
 let vPerson = document.getElementById('person'),
     vAppChat= document.getElementById('app-chat'),
     vWelcomeRutan= document.getElementById('welcome-rutan'),
-    vUser = document.getElementById('usr');
+    vUser = document.getElementById('usr'),
+    vMsg = document.getElementById('msg'),
+    vButtonSend = document.getElementById('send'),
+    vWrittingMsg = document.getElementById('writting-msg'),
+    vOutput = document.getElementById('output');
+//
+vButtonSend.addEventListener('click', function (e) {
+  if(vMsg.value){
+    socket.emit('chat',{
+      msg: vMsg.value,
+      user: vUser.value });
+    }
+  vMsg.value = '';
+
+});
+//msg som kommer från server
+socket.on('chat',function(data){
+  vWrittingMsg.innerHTML = '';
+  vOutput.innerHTML += `<p><strong> ${data.user} </strong> ${data.msg} </p>`;
+})
 
 function goIn(){
   if(vPerson.value){
