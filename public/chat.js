20122c155757614209a4a67e1eada3b1eb01db64
rsoriano <rsorianosanchis@gmail.com>
2019-05-25 18:04:43 +0200

    lördag 25/05/2019--18:03--frontend login,rummet av enkelt mono-chat-sala

--- public/chat.js
+++ public/chat.js
@@ -1,8 +1,15 @@
 'use strict';
 let vPerson = document.getElementById('person'),
     vAppChat= document.getElementById('app-chat'),
-    vWelcomeRutan= document.getElementById('welcome-rutan')
+    vWelcomeRutan= document.getElementById('welcome-rutan'),
+    vUser = document.getElementById('usr');
 
 function goIn(){
-  alert('hola');
+  if(vPerson.value){
+    vWelcomeRutan.style.display= 'none';
+    vAppChat.style.display = 'block';
+    let userNamn = vPerson.value;
+    vUser.value = userNamn;
+    vUser.readOnly = true;
+  }
 }
